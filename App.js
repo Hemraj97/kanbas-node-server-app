d5da70d86597fe1d1fcfacf044932747145500d0
hemraj97 <mahadeshwar.h@northeastern.edu>
2024-04-18 11:29:36 -0400

    Changes for Users

--- App.js
+++ App.js
@@ -1,17 +1,63 @@
-import express from "express";
+import "dotenv/config";
+import express from 'express';
+import mongoose from "mongoose";
 import Hello from "./Hello.js";
 import Lab5 from "./lab5.js";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Module/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/router.js";
 import cors from "cors";
-const app = express();
+import session from "express-session";
+import UserRoutes from "./Users/route.js";
 
-app.use(cors());
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+const app = express();
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+    }
+));
 app.use(express.json());
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  };  
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+      domain: process.env.HTTP_SERVER_DOMAIN,
+    };
+  }
+  app.use(
+    session({
+        secret: "keyboard cat",
+        resave: false,
+        saveUninitialized: false,
+        proxy: true,
+        cookie: {
+        sameSite: "none",
+        secure: true,
+        domain: process.env.HTTP_SERVER_DOMAIN,
+    },
+    })
+    );
+
+
 AssignmentRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
+UserRoutes(app);
 
 Lab5(app);
 Hello(app);
